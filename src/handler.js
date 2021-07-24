15c9c46fbc2ba37b1ae8ea2a52e7e291b34f538e
rizalarfiyan <rizal.arfiyan.23@gmail.com>
2021-07-24 08:13:15 +0700

    get all books

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,7 @@
 import { nanoid } from 'nanoid'
 import books from './books.js'
 
-export default function addBookHandler(request, h) {
+export function addBookHandler(request, h) {
   const {
     name,
     year,
@@ -69,4 +69,42 @@ export default function addBookHandler(request, h) {
       data: { bookId: id },
     })
     .code(201)
+}
+
+export function getAllBooksHandler(request, h) {
+  let filterBooks = [...books]
+  const { name, reading, finished } = request.query
+
+  if (typeof name !== 'undefined' && name.trim().length > 0) {
+    filterBooks = filterBooks.filter((book) =>
+      book.name.toLowerCase().match(new RegExp(name.toLowerCase(), 'gi'))
+    )
+  }
+
+  const numReading = Number.parseInt(reading, 10)
+  if (!Number.isNaN(numReading)) {
+    filterBooks = filterBooks.filter(
+      (book) => book.reading === Boolean(numReading)
+    )
+  }
+
+  const numFinished = Number.parseInt(finished, 10)
+  if (!Number.isNaN(numFinished)) {
+    filterBooks = filterBooks.filter(
+      (book) => book.finished === Boolean(numFinished)
+    )
+  }
+
+  return h
+    .response({
+      status: 'success',
+      data: {
+        books: filterBooks.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    })
+    .code(200)
 }
